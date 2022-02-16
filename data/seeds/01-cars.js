e3aa02550437a723acec16bab5ab9f8ea37f6d74
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-15 21:02:30 -0600

    seed done

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+  {
+    vin: '12345678911111111',
+    make: 'lada',
+    model: 'samara',
+    mileage: 666000,
+    title: 'clean',
+    transmission: 'manual'
+  },
+  {
+    vin: '12345678911111111',
+    make: 'vw',
+    model: 'jetta',
+    mileage: 120000,
+    title: 'salvage',
+  },
+  {
+    vin: '12345678911111111',
+    make: 'chevy',
+    model: 'corolla',
+    mileage: 666
+  }
+]
+
+exports.seed = function(knex){
+  return knex('cars')
+  .truncate().then(()=>{
+    return knex('cars').insert(cars)
+  })
+}
