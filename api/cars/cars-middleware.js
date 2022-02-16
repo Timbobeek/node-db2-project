c31c1e875260b882380481e4f90f06b7cc835f9c
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-15 22:38:02 -0600

    another md done

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,34 +1,60 @@
-const Car = require('./cars-model')
+const Car = require("./cars-model");
+const vin = require("vin-validator");
 
 const checkCarId = async (req, res, next) => {
   try {
-    const car = await Car.getById(req.params.id)
-    if (!car){
-      next({status:404, message: 'not found'})   ///we use next so that we go to the error logger md (and gives us stack in addition to error message)
+    const car = await Car.getById(req.params.id);
+    if (!car) {
+      next({ status: 404, message: "car with id this is not found" }); ///we use next so that we go to the error logger md (and gives us stack in addition to error message)
     } else {
-      next()
+      req.car = car; ///need this explained!
+      next();
     }
-  } catch (err){
-    next(err)
+  } catch (err) {
+    next(err);
   }
-}
+};
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  if (!req.body.vin)
+    return next({
+      status: 400,
+      message: "vin is missing",
+    });
+  if (!req.body.make)
+    return next({
+      status: 400,
+      message: "make is missing",
+    });
+  if (!req.body.model)
+    return next({
+      status: 400,
+      message: "model is missing",
+    });
+  if (!req.body.mileage)
+    return next({
+      status: 400,
+      message: "mileage is missing",
+    });
+  next();
+};
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  // const {vin2} = req.body
+  if (vin.validate(req.body.vin)) {
+    next();
+  } else {
+    next({ status: 400, message: `vin ${req.body.vin} is invalid` });
+  }
+};
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
-}
-
+};
 
 module.exports = {
   checkCarId,
   checkCarPayload,
   checkVinNumberValid,
-  checkVinNumberUnique
-}
+  checkVinNumberUnique,
+};
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,35 +1,36 @@
-const express = require('express')
-const Car = require('./cars-model')
+const express = require("express");
+const Car = require("./cars-model");
 
-const{
+const {
   checkCarId,
   checkCarPayload,
   checkVinNumberUnique,
-  checkVinNumberValid
-} = require('./cars-middleware')
+  checkVinNumberValid,
+} = require("./cars-middleware");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next)=>{
+router.get("/", async (req, res, next) => {
   try {
-    const cars = await Car.getAll()
-    res.json(cars)
-  } catch (err){
-    next(err)
+    const cars = await Car.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
   }
-})
+});
 
-router.get('/:id', checkCarId, async (req, res, next)=>{
-  try {
-    const car = await Car.getById(req.params.id)
-    res.json(car)
-  } catch (err){
-    next(err)
+router.get("/:id", checkCarId, async (req, res, next) => {
+  res.json(req.car);
+});
+
+router.post(
+  "/",
+  checkCarPayload,
+  checkVinNumberValid,
+  checkVinNumberUnique,
+  async (req, res, next) => {
+    res.json("stuff");
   }
-})
-
-router.post('/', async (req, res, next)=>{
-  res.json('stuff')
-})
+);
 
 module.exports = router;
