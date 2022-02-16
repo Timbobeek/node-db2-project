0e59844227ee9e38589f789e8009c485a595ff57
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-15 19:20:47 -0600

    first stuff

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,15 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('car_id')
+    tbl.text('car_vin').unique().notNullable()
+    tbl.text('car_make').notNullable()
+    tbl.text('car_model').notNullable()
+    tbl.integer('car_mileage').notNullable()
+    tbl.text('car_title')
+    tbl.text('car_transmission')
+  })
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
